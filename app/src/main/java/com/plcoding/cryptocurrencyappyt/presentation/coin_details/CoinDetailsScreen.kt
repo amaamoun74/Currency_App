9c2103e0e402a5c7e72e6466fc630815c91d8303
Ahmed Maamoun <amaamoun74@gmail.com>
2022-10-27 01:54:08 +0200

    Initial commit

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/coin_details/CoinDetailsScreen.kt
@@ -0,0 +1,12 @@
+package com.plcoding.cryptocurrencyappyt.presentation.coin_details
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun CoinDetailsScreen(
+    viewModel: CoinDetailsViewModel = hiltViewModel()
+){
+
+
+}
